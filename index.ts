ec710f54c33d0829d5710d71c51869c54e39336b
M HUZAIFA RAEES <153712158+mhuzaifaraees@users.noreply.github.com>
2024-05-20 12:17:49 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/npm login
+
+import inquirer from "inquirer";
+
+const asnwer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter secand number", type: "number", name: "secandNumber" },
+  {
+    message: "select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+/////conditional statement
+if (asnwer.operator === "Addition") {
+    console.log( asnwer.firstNumber + asnwer.secandNumber);
+}
+
+else if (asnwer.operator === "Subtraction") {
+    console.log( asnwer.firstNumber - asnwer.secandNumber);
+}
+
+else if (asnwer.operator === "Multiplication") {
+    console.log( asnwer.firstNumber * asnwer.secandNumber);
+}
+
+else if (asnwer.operator === "Division") {
+    console.log( asnwer.firstNumber / asnwer.secandNumber);
+}
+
+else {
+    console.log("please select valid operators")
+}
+
